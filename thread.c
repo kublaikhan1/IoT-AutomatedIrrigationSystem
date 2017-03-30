ee517867ea91af4bc71119f3118d916e9707ee4d
hamad Khawaja <khawajahamad217@gmail.com>
2017-03-30 18:24:30 -0400

    Threading implementation concept

---
+++ thread.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+// A normal C function that is executed as a thread when its name
+// is specified in pthread_create()
+void funct();
+
+void *soilMoisture(void *vargp)
+{
+     sleep(10);
+     funct();
+     return NULL;
+}
+
+int main()
+{
+     pthread_t pt;
+     int k=1;
+     pthread_create(&pt, NULL, soilMoisture, NULL);
+     while(k>0)
+     {
+       printf("This is the main thread (Measuring the water leverl) : %d\n", k);
+       sleep(1);
+     }
+     return 0;
+}
+
+void funct()
+{
+  printf("******(Measuring soil moisture after sleeping for 10SEC)***********\n");
+  pthread_t ptk;
+  pthread_create(&ptk, NULL, soilMoisture, NULL);
+}
