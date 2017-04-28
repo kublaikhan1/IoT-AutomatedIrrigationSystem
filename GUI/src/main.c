ee6161f3d3862f1580c5303d75bec5ce61e08e2e
Elliot Giese <etg7125@gmail.com>
2017-04-28 13:42:08 -0400

    updated gui

--- GUI/src/main.c
+++ GUI/src/main.c
@@ -2,15 +2,23 @@
 #include <stdlib.h>
 
 GtkWidget *g_lbl_hello;
+GtkWidget *btn_START;
+GtkWidget *btn_STOP;
+
+int ran = 0;
 
 int main(int argc, char *argv[])
 {
     GtkBuilder      *builder; 
     GtkWidget       *window;
 
+
     gtk_init(&argc, &argv);
 
     builder = gtk_builder_new();
+    
+ 
+    
     gtk_builder_add_from_file (builder, "glade/window_main.glade", NULL);
 
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
@@ -28,12 +36,22 @@ int main(int argc, char *argv[])
 
 void on_btn_START_clicked()
 {	
+	
 	gtk_label_set_text(GTK_LABEL(g_lbl_hello), "ON");
+	if(ran == 0)
+	{
+		//ran = system("./myprog");
+		//printf("%d\n", ran);
+		//ran = 1;
+		//printf("%d\n", ran);
+	}
+	
 }
 
 void on_btn_STOP_clicked()
 {
     gtk_label_set_text(GTK_LABEL(g_lbl_hello), "OFF");
+    //ran = 0;
 }
 
 // called when window is closed
